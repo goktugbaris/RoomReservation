312ef893bdd703f81c366f2c71c22fcdb101c11d
unknown <goktugbaris02@gmail.com>
2022-07-20 15:20:55 +0300

    Validator Added

--- RoomReservation.API/Startup.cs
+++ RoomReservation.API/Startup.cs
@@ -23,7 +23,6 @@ namespace RoomReservation.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IGenericRepository, GenericRepository>();
             services.AddRazorPages();
         }
 
---
+++ RoomReservation.Business/Validators/RoomValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using RoomReservation.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomReservation.Business.Validators
+{
+    public class RoomValidator:AbstractValidator<Room>
+    {
+    }
+}
